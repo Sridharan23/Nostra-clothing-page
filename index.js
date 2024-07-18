2f74064d9b13a7455160e793b89eeb3c92a48cee
Sridharan23 <sri20032003@gmail.com>
2024-07-18 14:57:20 -0700

    new

---
+++ index.js
@@ -0,0 +1,6 @@
+let closeoffer = document.querySelector(".close")
+let offer = document.querySelector(".offer")
+
+closeoffer.addEventListener("click",function(){
+    offer.style.display = "none"
+})
